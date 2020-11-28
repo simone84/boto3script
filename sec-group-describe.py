a9f76bb3f506aa0a24cb893c0d677b1f4d77182c
Simone Antolini <simone.antolini@rbs.co.uk>
2020-11-28 18:50:25 +0000

    adding loop

--- sec-group-describe.py
+++ sec-group-describe.py
@@ -1,22 +1,25 @@
 #!/usr/local/bin/python3
 
 import boto3
+import sys
 from botocore.exceptions import ClientError
 
 ec2 = boto3.client('ec2')
 ec2b = boto3.resource('ec2')
 
-with open('sg.id_list', 'r') as sg_list:
-    for record in sg_list:
-        try:
-            print("This is the security group:", record)
-            response = ec2.describe_security_groups(GroupIds=[record])
-            sg_description = ec2b.SecurityGroup(record)
-            print()
-            print(response)
-            print()
-            print(sg_description.description + " and the groupname is " + sg_description.group_name)
-            print("VPC_ID:", sg_description.vpc_id)
+sg = open("sg.id_list", "r")
+for record in sg:
+    try:
+        record = record.rstrip("\n")
+        print("This is the security group:", record)
+        response = ec2.describe_security_groups(GroupIds=[record])
+        sg_description = ec2b.SecurityGroup(record)
+        print()
+        print(response)
+        print()
+        print(sg_description.description + " and the groupname is " + sg_description.group_name)
+        print("VPC_ID:", sg_description.vpc_id)
+        print()
 
-        except ClientError as e:
-            print(e)
+    except ClientError as e:
+        print(e)
