2817be2a980c63218007474743bc451c351f7de9
Simone Antolini <simone.antolini@rbs.co.uk>
2020-11-29 00:04:36 +0000

    adding vpc tags

--- check-resource-to-tag.py
+++ check-resource-to-tag.py
@@ -5,12 +5,11 @@ import sys
 import os
 from botocore.exceptions import ClientError
 ec2 = boto3.resource('ec2')
-security_group = ec2.SecurityGroup('id')
-
 
 # Security-groups
-os.system('aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId]" --output text > sg.id_list')
-sg = open("sg.id_list", "r")
+security_group = ec2.SecurityGroup('id')
+os.system('aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId]" --output text > sg-id_list')
+sg = open("sg-id_list", "r")
 for record in sg:
     try:
         record = record.rstrip("\n")
@@ -30,5 +29,29 @@ for record in sg:
         )
     except ClientError as e:
         print(e)
+os.remove("sg-id_list")
 
-os.remove("sg.id_list")
+# VPC
+vpc = ec2.Vpc('id')
+os.system('aws ec2 describe-vpcs --query "Vpcs[*].[VpcId]" --output text > vpc-id_list')
+vpc = open("vpc-id_list", "r")
+for record in vpc:
+    try:
+        record = record.rstrip("\n")
+        vpc_description = ec2.Vpc(record)
+        print("Adding tags on VPC:", record)
+        vpc_description.create_tags(
+            Tags=[
+                    {
+                    'Key': 'Boto3',
+                    'Value': 'True'
+                    },
+                    {
+                    'Key': 'TagAutoapply',
+                    'Value': 'True'
+                    }
+            ]
+        )
+    except ClientError as e:
+        print(e)
+os.remove("vpc-id_list")
