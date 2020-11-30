fedb9f9aaea9f387e5cbec93a9587e51e21d1d72
Simone Antolini <simone.antolini@rbs.co.uk>
2020-11-30 23:51:01 +0000

    correcting vpc loop

--- vpc-describe.py
+++ vpc-describe.py
@@ -8,13 +8,14 @@ from botocore.exceptions import ClientError
 os.system('aws ec2 describe-vpcs --query "Vpcs[*].[VpcId]" --output text > vpc-id_list')
 
 ec2 = boto3.resource('ec2')
+vpc = open("vpc-id_list", "r")
 
-sg = open("vpc-id_list", "r")
-for record in sg:
+print("VPC List:")
+
+for record in vpc:
     try:
         record = record.rstrip("\n")
         vpc_description = ec2.Vpc(record)
-        print("VPC List:")
         print("VPC-ID:", record, "with this CIDR:", vpc_description.cidr_block)
     except ClientError as e:
         print(e)
